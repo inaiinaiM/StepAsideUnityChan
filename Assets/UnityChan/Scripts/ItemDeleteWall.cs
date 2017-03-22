2217cb90e30f3e5290e6d550068b37e01914e7a5
Nriko Kobari <chomerico777pd@gmail.com>
2017-03-22 14:56:16 +0900

    Lesson6課題 Unitychanの後ろに見えない壁（ItemDeliteWall）を作成し、衝突したオブジェクトが破棄されるようにしました。

---
+++ Assets/UnityChan/Scripts/ItemDeleteWall.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDeleteWall : MonoBehaviour {
+
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+	//UnityちゃんとWallの距離
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+		
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find ("unitychan");
+		//Unityちゃんとwallの位置（z座標）の差を求める
+		this.distance = unitychan.transform.position.z - this.transform.position.z;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Unityちゃんの位置に合わせてwallの位置を移動
+		this.transform.position = new Vector3 (0, this.transform.position.y, this.unitychan.transform.position.z - distance);
+		
+	}
+
+	//トリガーモードで他のオブジェクトと接触した場合の処理
+	void OnTriggerEnter(Collider other) {
+
+		//objectに衝突した場合
+		if (other.gameObject.tag == "CoinTag" || other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag") {
+			//接触したオブジェクトを破棄
+			Destroy (other.gameObject);
+		}
+}
+}
